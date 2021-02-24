e018a07d2ddf950aa0cc99df605bc661c42e8a07
Besim Gjidija <besimgjidija@gmail.com>
2021-02-24 14:58:28 -0500

    Added messaging event listener to js

--- js/app.js
+++ js/app.js
@@ -126,3 +126,21 @@ let mobileChart = new Chart(mobileCanvas, {
 
 //---- Messaging Section ----
 
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+// click event listener on send
+
+send.addEventListener('click', () => {
+    // ensure user and message fileds are filled out 
+    if(user.value === "" && message.value === "") {
+        alert("Please fill out user and message fields before sending");
+    } else if(user.value === "") {
+        alert("Please fill out user field before sending")
+    } else if(message.value === "") {
+        alert("Please fill out message field before sending")
+    } else {
+        alert(`Message successfully sent to: ${user.value}`)
+    }
+});
